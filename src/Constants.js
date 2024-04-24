060d931a7e05a37d68e566dde50e090320e997b2
Mahak Agarwal <mahakagarwal248@gmail.com>
2024-04-24 22:25:49 +0530

    backend url updated

--- src/Constants.js
+++ src/Constants.js
@@ -2,7 +2,7 @@ export const PLATFORM_NAME = "InsightLearn";
 // export const baseURL = "http://localhost:8000/node/api/olp";
 // export const baseURL = "http://api.easycoder.tech/olp";
 export const baseURL =
-  "https://online-learning-platform-backend-mu.vercel.app/node/api/olp";
+  "https://online-learning-platform-backend-production.up.railway.app/node/api/olp";
 
 export const CONTACT_NUMBER = "+91 6396604215";
 export const CONTACT_EMAIL = "info@example.com";
